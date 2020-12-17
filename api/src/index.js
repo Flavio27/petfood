25fe665eb3cc721e997d5f537a8d9aced43eb864
Felipe Rocha <feliperocha.ti@gmail.com>
2020-12-17 16:27:20 -0300

    initial structure for the backend api

---
+++ api/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+
+require('./config/database')
+
+app.set('port', process.env.PORT || 8000)
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/', require('./routes/main.routes'));
+
+app.listen(app.get('port'), () => { console.log ('Server is up!')});
