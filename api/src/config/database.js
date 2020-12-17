90da1dbd28de5300525b83976a09f7e2fb476cc3
Felipe Rocha <feliperocha.ti@gmail.com>
2020-12-17 18:11:22 -0300

    fix DATABASE_URL call

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
-const { DATABASE_URL } = process.env.DATABASE_URL;
-
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('debug', true);
 
 mongoose
-  .connect(DATABASE_URL)
+  .connect(process.env.DATABASE_URL)
   .then(() => console.log('DB is up'))
   .catch((err) => console.log(`Error in DB initialization! error: ${err}`));
