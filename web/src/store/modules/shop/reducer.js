7d7584ba307bacde7261eef5a99f83773aa58438
Flavio Rocha <flavio_271295@hotmail.com>
2020-12-18 22:10:49 -0300

    add redux

---
+++ web/src/store/modules/shop/reducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {}
+
+function shop (state = INITIAL_STATE, action){
+	switch(action.type){
+		case 'SET_COSTUMER':{
+			//logica
+			break;
+		}
+		default:
+			return state;
+
+		
+	}
+
+}
