7dc8643a186e861dccfadee5f51e0f8c86b89323
Flavio Rocha <flavio_271295@hotmail.com>
2020-12-21 21:08:41 -0300

    add total price in the cart

--- web/src/components/sidebar/index.js
+++ web/src/components/sidebar/index.js
@@ -7,6 +7,9 @@ import './styles.css'
 
 const SideBar = () => {
 	const { cart } = useSelector((state) => state.shop)
+	const total = cart.reduce((total, product) =>{
+		return total + product.preco;
+	},0)
 	const [opened, setOpened] = useState(false);
 
 	useEffect(() => {
@@ -31,7 +34,7 @@ const SideBar = () => {
 				<div className="row align-items-end footer">
 					<div className="col-12 d-flex justify-content-between align-items-center">
 						<b className="d-inline-block">Total</b>
-						<h3 className="d-inline-block">R$ 90,00</h3>
+						<h3 className="d-inline-block">R$ {total.toFixed(2)}</h3>
 					</div>
 					<button className="btn btn-block btn-lg btn-primary rounded-0 h-50 w-100 align-items-center ">
 						Finalizar Compra
